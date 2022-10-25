1914c095cf7e959f337ee1ca1e1311b8915bf6d6
Kanumone <KuzminAAwork@yandex.ru>
2022-10-25 22:43:16 +0700

    Create config and app entry point

---
+++ app/src/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'config.php';
+require_once ROOT . '/vendor/autoload.php';
+require_once SRC_PATH . '/web.php';
---
+++ app/src/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const ROOT = "/var/www/html";
+const PUBLIC_PATH = ROOT . '/public';
+const SRC_PATH = ROOT . '/src';
+const TEMPLATES_PATH = SRC_PATH . '/resources/views';
