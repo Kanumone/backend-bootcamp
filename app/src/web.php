a2fc9230abbda4fc40ad705e627d9312cdf6a72d
Kanumone <KuzminAAwork@yandex.ru>
2022-10-29 10:23:34 +0700

    Add routes

--- app/src/web.php
+++ app/src/web.php
@@ -2,9 +2,10 @@
 
 namespace Phroute\Phroute;
 
-function processInput($uri){
-    $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    return $uri;
+use Kanumone\Bshop\Controllers\SectionController;
+
+function processInput(){
+    return urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 }
 
 function processOutput($response){
@@ -13,14 +14,16 @@ function processOutput($response){
 
 $router = new RouteCollector();
 
-$router->get('/', function(){
-    return 'Like at Home!';
+$router->get('/', ['\Kanumone\Bshop\Controllers\IndexController', 'index']);
+$router->get('/section/{section_id}', function ($section_id) {
+   $SectionController = new SectionController();
+   $SectionController->show($section_id);
 });
 
 $dispatcher = new Dispatcher($router->getData());
 
 try {
-    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], processInput($_SERVER['REQUEST_URI']));
+    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], processInput());
 } catch (Exception\HttpRouteNotFoundException $e) {
     http_response_code(404);
     echo "Not found";
