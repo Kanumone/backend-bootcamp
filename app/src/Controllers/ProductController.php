e7a1ff3a828e2ebe4ec7fe7790f3fe217fc80e9c
santiago <santiago@student.21-school.ru>
2022-10-29 17:26:25 +0700

    Add product main logic

--- app/src/Controllers/ProductController.php
+++ app/src/Controllers/ProductController.php
@@ -3,8 +3,20 @@
 namespace Kanumone\Bshop\Controllers;
 
 use Kanumone\Bshop\Core\Controller;
+use Kanumone\Bshop\Models\ProductModel;
 
 class ProductController extends Controller
 {
+    public function __construct($product_id)
+    {
+        parent::__construct();
+        $this->model = new ProductModel($product_id);
+    }
 
+    public function show() {
+        $this->pageData['product_info'] = $this->model->getProductInfo();
+        $this->pageData['images'] = $this->model->getImages();
+        $this->pageData['sections'] = $this->model->getSections();
+        $this->view->render('product_card', $this->pageData);
+    }
 }
--- app/src/Models/ProductModel.php
+++ app/src/Models/ProductModel.php
@@ -6,5 +6,45 @@ use Kanumone\Bshop\Core\Model;
 
 class ProductModel extends Model
 {
+    private int $product_id;
 
+    public function __construct($product_id) {
+        parent::__construct();
+        $this->product_id = $product_id;
+    }
+
+    public function getProductInfo() {
+        $req = 'select p.title,  round(p.price) as price, p.description
+                from products p
+                where product_id = :product_id';
+        $st = $this->db->prepare($req);
+        $st->execute([
+            "product_id" => $this->product_id
+        ]);
+        return $st->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getImages() {
+        $req = 'select i.path, i.alt, i.type from images i
+                join product_image pi
+                on pi.image_id = i.image_id
+                where pi.product_id = :product_id';
+        $st = $this->db->prepare($req);
+        $st->execute([
+            "product_id" => $this->product_id
+        ]);
+        return $st->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getSections() {
+        $req = 'select s.title from sections s
+	            join product_section ps
+                on ps.section_id = s.section_id
+                where ps.product_id = :product_id;';
+        $st = $this->db->prepare($req);
+        $st->execute([
+            "product_id" => $this->product_id
+        ]);
+        return $st->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
