2e3f7b7193d2a2ca1fc901fadb27e73225a04dda
santiago <santiago@student.21-school.ru>
2022-10-29 17:52:26 +0700

    Return main image on mouseout

--- app/public/js/changeImg.js
+++ app/public/js/changeImg.js
@@ -1,9 +1,13 @@
 const mainImg = document.querySelector('.main-img');
+const mainImgSrc = mainImg.getAttribute('src');
 const slider = document.querySelector('.slider');
 const imgs = Array.from(slider.children);
 imgs.forEach(img => {
+  const imgSrc = img.getAttribute('src');
   img.addEventListener('mouseover', () => {
-    if (img.classList.contains('first')) mainImg.setAttribute('src', './images/main.webp');
-    else mainImg.setAttribute('src', img.attributes[0].value);
+    mainImg.setAttribute('src', imgSrc);
+  });
+  img.addEventListener('mouseout', () => {
+    mainImg.setAttribute('src', mainImgSrc);
   });
 })
