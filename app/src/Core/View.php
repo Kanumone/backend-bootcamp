528d7021a7eb9cb3e5835a99934b1c1944a903ca
Kanumone <KuzminAAwork@yandex.ru>
2022-11-01 21:04:58 +0700

    Add format number and pagination

--- app/src/Core/View.php
+++ app/src/Core/View.php
@@ -10,4 +10,32 @@ class View
         require_once TEMPLATES_PATH . $template . '.php';
         require_once TEMPLATES_PATH . 'footer.php';
     }
+
+    public function formatPrice($price, $decimal = 0): string {
+        if (empty($price)) {
+            return '';
+        } else {
+            return number_format($price, $decimal, '.',' ');
+        }
+    }
+
+    public function pagination($amountItems, $itemPerPage) {
+        $countPages = $amountItems / $itemPerPage;
+        $html = '
+            <nav aria-label="pagination">
+              <ul class="pagination pagination-lg justify-content-center">
+        ';
+        for ($i = 1; $i <= $countPages; $i++) {
+            $html .= "
+                <li class=\"page-item\" aria-current=\"page\">
+                  <span class=\"page-link\">$i</span>
+                </li>
+            ";
+        }
+        $html .= '
+                </ul>
+            </nav>
+        ';
+        return $html;
+    }
 }
