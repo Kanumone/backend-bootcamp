3e85d9a3c444b1a7883b10b9ae510d9ccb53dc89
Kanumone <KuzminAAwork@yandex.ru>
2022-11-01 21:03:45 +0700

    Correct queries, add breadcrumbs

--- app/src/resources/views/section.php
+++ app/src/resources/views/section.php
@@ -1,22 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="<?= SITE_PATH . "/css/card.css" ?>">
-</head>
-<body>
-    <div class="container">
-        <?php foreach ($pageData['products'] as $id => $product) :?>
-            <div class="card">
-                <img src="<?= SITE_PATH . $product['image_path'] ?>" alt="<?= $product['image_alt'] ?>" class="card__preview"/>
-                <div class="card__price"><?= $product['price'] ?></div>
-                <div class="card__title"><?= $product['title'] ?></div>
-            </div>
-        <?php endforeach ;?>
+<link rel="stylesheet" href="<?= SITE_PATH . "css/card.css" ?>">
+
+<div class="card-list">
+<?php foreach ($pageData['products'] as $id => $product) :?>
+    <div class="card">
+        <div class="card__preview">
+            <img src="<?= SITE_PATH . $product['image_path'] ?>" alt="<?= $product['image_alt'] ?>"/>
+        </div>
+        <div class="card__section" style="text-align: end; padding: 4px"><?= $product['main_section_id'] ?></div>
+        <a href="<?= $_SERVER["REQUEST_URI"] . "/product/{$product['product_id']}" ?>">
+            <div class="card__title"><?= $product['title'] ?></div>
+        </a>
     </div>
-</body>
-</html>
+<?php endforeach ;?>
+</div>
+
+<?= $this->pagination(15, 5); ?>
