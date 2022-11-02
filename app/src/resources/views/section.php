e30f35452f42ee09defb598c0f16f139924c8735
Kanumone <KuzminAAwork@yandex.ru>
2022-11-02 09:20:51 +0700

    Fix bug with link

--- app/src/resources/views/section.php
+++ app/src/resources/views/section.php
@@ -6,12 +6,10 @@
         <div class="card__preview">
             <img src="<?= SITE_PATH . $product['image_path'] ?>" alt="<?= $product['image_alt'] ?>"/>
         </div>
-        <div class="card__section" style="text-align: end; padding: 4px"><?= $product['main_section_id'] ?></div>
-        <a href="<?= $_SERVER["REQUEST_URI"] . "/product/{$product['product_id']}" ?>">
+        <div class="card__section" style="text-align: end; padding: 4px"><?= $product['main_section'] ?></div>
+        <a href="<?= parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . "/product/{$product['product_id']}" ?>">
             <div class="card__title"><?= $product['title'] ?></div>
         </a>
     </div>
 <?php endforeach ;?>
 </div>
-
-<?= $this->pagination(15, 5); ?>
