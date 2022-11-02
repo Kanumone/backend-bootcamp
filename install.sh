e12b6a62a5948933b64798a4c0d444d54f20b502
Kanumone <KuzminAAwork@yandex.ru>
2022-11-02 22:48:56 +0700

    Delete db

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cat db/dump.sql | docker exec -i bootcamp_db /usr/bin/mysql -u root --password=root bootcamp
+cat db/dump.sql | docker exec -i bootcamp_db /usr/bin/mysql -u root --password=root
 cd ./app
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
